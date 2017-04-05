ff8990b538eefc048fa823d0feaef17454a6fa7d
TheBrood <aeiler16@student.aau.dk>
2017-04-05 10:57:01 +0000

    Hello this is more sigmoid stuff, have a nice day

--- ArtificialNeuralNetwork/ArtificialNeuralNetwork/TranferFunction.cs
+++ ArtificialNeuralNetwork/ArtificialNeuralNetwork/TranferFunction.cs
@@ -32,4 +32,82 @@ class TransferFunction
 
 		return result;
 	}
+
+	public double[] Tahn(double[] inputArray, double[,] weightArray)
+	{
+		double[] result = new double[weightArray.GetLength(0)];
+
+		int k = 0;
+
+		for (int i = 0; i < weightArray.GetLength(0); i++)
+		{
+			foreach (double input in inputArray)
+			{
+				result[k] += input * weightArray[i, k];
+				k++;
+			}
+
+		}
+
+		foreach (double res in result)
+		{
+			k = 0;
+			result[k] = 2 / (1 + Exp(-2 * res)) - 1;
+			k++;
+		}
+
+		return result;
+	}
+
+	public double[] Hyperbolic(double[] inputArray, double[,] weightArray)
+	{
+		double[] result = new double[weightArray.GetLength(0)];
+
+		int k = 0;
+
+		for (int i = 0; i < weightArray.GetLength(0); i++)
+		{
+			foreach (double input in inputArray)
+			{
+				result[k] += input * weightArray[i, k];
+				k++;
+			}
+
+		}
+
+		foreach (double res in result)
+		{
+			k = 0;
+			result[k] = (Exp(res) - Exp(-res)) / (Exp(res) + Exp(-res));
+			k++;
+		}
+
+		return result;
+	}
+
+	public double[] Linear(double[] inputArray, double[,] weightArray)
+	{
+		double[] result = new double[weightArray.GetLength(0)];
+
+		int k = 0;
+
+		for (int i = 0; i < weightArray.GetLength(0); i++)
+		{
+			foreach (double input in inputArray)
+			{
+				result[k] += input * weightArray[i, k];
+				k++;
+			}
+
+		}
+
+		/*foreach (double res in result)
+		{
+			k = 0;
+			result[k] = 1 / (1 + Exp(-res));
+			k++;
+		}*/
+
+		return result;
+	}
 }
