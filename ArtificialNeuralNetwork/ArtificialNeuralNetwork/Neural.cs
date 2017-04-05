43b6ef11193da7518affb3d4667df6b7dcee4751
MrLongbottom <perich16@student.aau.dk>
2017-04-05 10:05:04 +0200

    began work on Forward() and Cycle()

--- ArtificialNeuralNetwork/ArtificialNeuralNetwork/Neural.cs
+++ ArtificialNeuralNetwork/ArtificialNeuralNetwork/Neural.cs
@@ -10,24 +10,49 @@ namespace ArtificialNeuralNetwork
     {
         //Variables
         public Layer[] layers;
+        public int inputSize;
 
         //Constructor
         public NeuralNetwork(int[] size)
         {
-            layers = new Layer[size.Length];
-            for (int i = layers.Length; i > 0; i--)
+            inputSize = size[0];
+            layers = new Layer[size.Length-1];
+            for (int i = 0; i < layers.Length; i++)
             {
-                Layer nextLayer;
-                if (i == layers.Length)
-                {
-                    nextLayer = null;
-                }
-                else
-                {
-                    nextLayer = layers[i + 1];
-                }
-                layers[i] = new Layer(size[i], nextLayer);
+                layers[i] = new Layer(size[i+1], size[i]);
             }
+        }
+
+        public double[] Cycle (double[] input)
+        {
+            if (input.Length != inputSize)
+            {
+                throw new Exception($"Wrong input array size, expected {inputSize}, but got {input.Length}.");
+            }
+            else
+            {
+                double[] data = input;
+                for (int i = 0; i < layers.Length; i++)
+                {
+                    data = Forward(data, layers[i]);
+                }
+                return data;
+            }
+        }
+
+        public double[] Forward (double[] input, Layer currLayer)
+        {
+            double[] ret = new double[currLayer.weights.GetLength(0)];
+            for (int i = 0; i < ret.Length; i++)
+            {
+                double curr = 0;
+                for (int i2 = 0; i2 < input.Length; i++)
+                {
+                    curr += input[i2] * currLayer.weights[i, i2];
+                }
+                ret[i] = curr;
+            }
+            return ret;
         }
     }
 }
