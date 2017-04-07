b4e4900f81f8018443891d270fff0ca0dac51706
Rasmus Christensen <slayeren3024@gmail.com>
2017-04-07 09:36:59 +0200

    Load Data Projekt

---
+++ LoadDataTest/LoadDataTest/LoadingData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LoadDataTest
+{
+    class LoadingData
+    {
+
+        public string[] GetFileNames()
+        {            
+            string[] files = Directory.GetFiles(@"C:\Users\Rasmus\Desktop\Python\Backup\Data\CSV Files Champion Data\", "*.csv");
+
+            for (int index = 0; index < files.Length; index++)
+            {
+                files[index] = Path.GetFileName(files[index]);
+            }
+            
+            foreach (var item in files)
+            {
+                Console.WriteLine(item);
+            }
+            
+            return files;    
+        }
+
+        
+
+        public string[,,] LoadCsv(string path)
+        {
+
+            // Get the file's text.
+            string CurrentFile = File.ReadAllText(path);
+
+            // Split into lines.
+            CurrentFile = CurrentFile.Replace('\n', '\r');
+            string[] lines = CurrentFile.Split(new char[] { '\r' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            // See how many rows and columns there are.
+            int num_rows = lines.Length;
+            int num_cols = lines[0].Split(',').Length;
+            int num_files = GetFileNames().Length;
+
+            // Allocate the data array.
+            string[,,] values = new string[num_rows, num_cols, num_files];
+
+            // Load the array.
+            for (int r = 0; r < num_rows; r++)
+            {
+                string[] line_r = lines[r].Split(',');
+                for (int c = 0; c < num_cols; c++)
+                {
+                    values[r, c, 0] = line_r[c];
+                }
+            }
+
+            // Return the values.
+            return values;
+        }
+
+        public void PrintArray(string[,,] values)
+        {
+            foreach (var item in values)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ LoadDataTest/LoadDataTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LoadDataTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,,] Data;
+            LoadingData Load = new LoadingData();
+            int NumberOfFiles = Load.GetFileNames().Length;
+            Data = Load.LoadCsv(@"C:\Users\Rasmus\Desktop\Python\Backup\Data\CSV Files Champion Data\EU 2016 Spring Champion Statistics.csv");
+            Load.PrintArray(Data);
+            //Load.GetFileNames();
+            Console.ReadKey();
+        }
+    }
+}
