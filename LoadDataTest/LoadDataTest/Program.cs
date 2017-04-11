ab01454c8e5a18bf498c0b955c544cf64787b1ae
Rasmus Christensen <slayeren3024@gmail.com>
2017-04-11 14:09:00 +0200

    EVERTHING WORKS!

--- LoadDataTest/LoadDataTest/Program.cs
+++ LoadDataTest/LoadDataTest/Program.cs
@@ -11,13 +11,39 @@ namespace LoadDataTest
     {
         static void Main(string[] args)
         {
-            string[,,] Data;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            // the code that you want to measure comes here
+
+            // New instance of Loading data
             LoadingData Load = new LoadingData();
-            int NumberOfFiles = Load.GetFileNames().Length;
-            Data = Load.LoadCsv(@"C:\Users\Rasmus\Desktop\Python\Backup\Data\CSV Files Champion Data\EU 2016 Spring Champion Statistics.csv");
-            Load.PrintArray(Data);
-            //Load.GetFileNames();
+
+            // Data Paths
+            string ChampionDataPath = @"C: \Users\Rasmus\Desktop\Uni\P2\Git\Projekt\Data\CSV Files Champion Data";
+            string EsportsDataPath = @"C:\Users\Rasmus\Desktop\Uni\P2\Git\Projekt\Data\CSV Files Lol Esport Data";
+            string MatchupsDataPath = @"C:\Users\Rasmus\Desktop\Uni\P2\Git\Projekt\Data\CSV Files Matchups";
+
+            // Number of files in datapaths
+            int NOFilesInChampion = Load.GetFileNames(ChampionDataPath).Length;
+            int NOFilesInEsports = Load.GetFileNames(EsportsDataPath).Length;
+            int NOFilesInMatchups = Load.GetFileNames(MatchupsDataPath).Length;
+
+            // New arrays with 2d array where the data should be stored
+            string[][,] ChampionData = new string[NOFilesInChampion][,];
+            string[][,] EsportsData = new string[NOFilesInEsports][,];
+            string[][,] MatchupsData = new string[NOFilesInMatchups][,];
+
+            // Loading data into the arrays
+            ChampionData = Load.LoadPath(ChampionDataPath);
+            EsportsData = Load.LoadPath(EsportsDataPath);
+            MatchupsData = Load.LoadPath(MatchupsDataPath);
+
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine("done it took " + elapsedMs + " milliseconds to load the data");           
             Console.ReadKey();
+
         }
     }
 }
