0c05f754d3769b64d55f54eb72aaad713b9e77c1
EbbeLau <ebarge16@student.aau.dk>
2017-04-13 15:32:26 +0200

    Make file

---
+++ ArtificialNeuralNetwork/ArtificialNeuralNetwork/Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtificialNeuralNetwork
+{
+    class Tree
+    {
+        int GetChances(int BestOf, int HandicapHome, int HandicapOut, double ChanceHome, double ChanceOut)
+        {
+
+            return 0;
+        }
+
+        double GetSingleTeamChance(int HomeWon, int OutWon, int BestOf, double ChanceHome, double ChanceOut)
+        {
+
+            return 0;
+        }
+    }
+}
