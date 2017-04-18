2136890dfd0dffcbd0e2bae7e41d7053e7fad74e
MrLongbottom <perich16@student.aau.dk>
2017-04-18 13:54:07 +0200

    Bias fixed

--- ArtificialNeuralNetwork/ArtificialNeuralNetwork/Layer.cs
+++ ArtificialNeuralNetwork/ArtificialNeuralNetwork/Layer.cs
@@ -10,15 +10,18 @@ namespace ArtificialNeuralNetwork
     {
         //Variables
         public double[,] weights;
-        public double[] neurons;
+        public double[] activations;
+        public double[] zums;
 
         //Constructor
         public Layer(int size, int prevSize, Random rand)
         {
-            neurons = new double[size];
-            for (int i = 0; i < neurons.Length; i++)
+            activations = new double[size];
+            zums = new double[size];
+            for (int i = 0; i < size; i++)
             {
-                neurons[i] = 0;
+                activations[i] = 0;
+                zums[i] = 0;
             }
             weights = new double[size, prevSize+1];
             for(int i = 0; i < weights.GetLength(0); i++)
--- ArtificialNeuralNetwork/ArtificialNeuralNetwork/Neural.cs
+++ ArtificialNeuralNetwork/ArtificialNeuralNetwork/Neural.cs
@@ -31,6 +31,14 @@ namespace ArtificialNeuralNetwork
             }
         }
 
+        //Tomt neuralt netværk constructor til objectorienteret/dynamisk skabelse
+        public NeuralNetwork(TranferFunction activationFunction, TranferFunction outputFunction)
+        {
+            _activationFunction = activationFunction;
+            _outputFunction = outputFunction;
+            layers = new Layer[0];
+        }
+
         //Functions
 
         public void AddLayer(Layer lay)
@@ -61,39 +69,38 @@ namespace ArtificialNeuralNetwork
 
         public double[] Cycle (double[] input)
         {
-            //double check if bias works!
             if (input.Length != inputSize)
             {
                 throw new Exception($"Wrong input array size, expected {inputSize}, but got {input.Length}.");
             }
             else
             {
-                //Set up bias node for input
-                double[] data = new double[input.Length + 1];
-                for (int i = 0; i < data.Length; i++)
-                {
-                    if (i == 0)
-                    {
-                        data[i] = 1;
-                    }
-                    else
-                    {
-                        data[i] = input[i-1];
-                    }
-                }
-                PrintArray("Data:",data);
+                double[] data = input;
                 //Cycle through network
                 for (int i = 0; i < layers.Length; i++)
                 {
+                    //Set up bias node for input
+                    double[] newdata = new double[data.Length + 1];
+                    for (int j = 0; j < newdata.Length; j++)
+                    {
+                        if (j == 0)
+                        {
+                            newdata[j] = 1;
+                        }
+                        else
+                        {
+                            newdata[j] = data[j - 1];
+                        }
+                    }
+                    data = newdata;
+                    PrintArray($"Data Layer {i}:", data);
                     //Use Transferfunction on all layers except the last
                     if (i != layers.Length - 1)
                     {
-                        PrintArray($"Pre-Data Layer {i}:", data);
                         //Find sums for each neuron
                         data = Sum(data, layers[i].weights);
                         //Use Transferfunction on each sum
                         data = _activationFunction(data);
-                        PrintArray($"Data Layer {i}:", data);
                     }
                     //Use Softmax on last layer
                     else
@@ -101,7 +108,7 @@ namespace ArtificialNeuralNetwork
                         data = Sum(data, layers[i].weights);
                         data = _outputFunction(data);
                         PrintArray("Data output:", data);
-                        Console.WriteLine($"Softmax: {data[0]+data[1]}");
+                        Console.WriteLine($"Softmax check: {data[0]+data[1]}");
                     }
                 }
                 //Possibility Tree
