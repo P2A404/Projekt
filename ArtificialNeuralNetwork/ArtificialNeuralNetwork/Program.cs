9ff6d5a4096bebd9938c5a2e77d0ca8641c9376c
Ebbe Lau Grøn Børgesen <ebarge16@student.aau.dk>
2017-04-20 05:38:42 +0200

    Sorry. Late commit. Tree done.

--- ArtificialNeuralNetwork/ArtificialNeuralNetwork/Program.cs
+++ ArtificialNeuralNetwork/ArtificialNeuralNetwork/Program.cs
@@ -10,21 +10,21 @@ namespace ArtificialNeuralNetwork
     {
         static void Main(string[] args)
         {
-            #region start
-            Console.Clear();
-            Console.WriteLine("Hello World!");
-            #endregion
-            TransferFunctions tf = new TransferFunctions();
-            NeuralNetwork NN1 = new NeuralNetwork(new int[] { 2, 3, 3, 2 }, tf.Logistic, tf.SoftMax);
-            NN1.PrintArray("Cycle:", NN1.Cycle(new double[] { 33.99521, 47.5 }));
-            NeuralNetwork NN2 = new NeuralNetwork(new int[] { 2, 3, 3, 2 }, tf.Tahn, tf.SoftMax);
-            NN2.PrintArray("Cycle:", NN2.Cycle(new double[] { 33.99521, 47.5 }));
-            NeuralNetwork NN3 = new NeuralNetwork(new int[] { 2, 3, 3, 2 }, tf.Hyperbolic, tf.SoftMax);
-            NN3.PrintArray("Cycle:", NN3.Cycle(new double[] { 33.99521, 47.5 }));
-            #region end
-            Console.WriteLine("Goodbye Cruel World.");
-            Console.ReadLine();
-            #endregion
+            //#region start
+            //Console.Clear();
+            //Console.WriteLine("Hello World!");
+            //#endregion
+            //TransferFunctions tf = new TransferFunctions();
+            //NeuralNetwork NN1 = new NeuralNetwork(new int[] { 2, 3, 3, 2 }, tf.Logistic, tf.SoftMax);
+            //NN1.PrintArray("Cycle:", NN1.Cycle(new double[] { 33.99521, 47.5 }));
+            //NeuralNetwork NN2 = new NeuralNetwork(new int[] { 2, 3, 3, 2 }, tf.Tahn, tf.SoftMax);
+            //NN2.PrintArray("Cycle:", NN2.Cycle(new double[] { 33.99521, 47.5 }));
+            //NeuralNetwork NN3 = new NeuralNetwork(new int[] { 2, 3, 3, 2 }, tf.Hyperbolic, tf.SoftMax);
+            //NN3.PrintArray("Cycle:", NN3.Cycle(new double[] { 33.99521, 47.5 }));
+            //#region end
+            //Console.WriteLine("Goodbye Cruel World.");
+            //Console.ReadLine();
+            //#endregion
         }
     }
 }
--- ArtificialNeuralNetwork/ArtificialNeuralNetwork/Tree.cs
+++ ArtificialNeuralNetwork/ArtificialNeuralNetwork/Tree.cs
@@ -10,12 +10,28 @@ namespace ArtificialNeuralNetwork
     {
         int GetChances(int BestOf, int HandicapHome, int HandicapOut, double ChanceHome, double ChanceOut)
         {
+            double HomeChance = 0;
+            double OutChance = 0;
+            double DrawChance = 0;
+            int TempBestOf = BestOf + HandicapHome + HandicapOut;
+            int PseudoRound = HandicapOut + HandicapHome;
+            HomeChance = GetSingleTeamChance(HandicapHome, HandicapOut, TempBestOf, ChanceHome, ChanceOut, PseudoRound);
+
+            OutChance = GetSingleTeamChance(HandicapOut, HandicapHome, TempBestOf, ChanceOut, ChanceHome, PseudoRound);
+
+            DrawChance = 1 - HomeChance - OutChance;
 
             return 0;
         }
 
-        double GetSingleTeamChance(int HomeWon, int OutWon, int BestOf, double ChanceHome, double ChanceOut)
+        double GetSingleTeamChance(int HomeWon, int OutWon, int BestOf, double ChanceHome, double ChanceOut, int Round)
         {
+            if (HomeWon == (BestOf/2) + 1) { return 1; }
+            else if (OutWon == (BestOf / 2) + 1 || Round == BestOf) { return 0; }
+            else
+            {
+                double ReturnValue = ChanceHome * GetSingleTeamChance(HomeWon, OutWon, BestOf, ChanceHome, ChanceOut, Round);
+            }
 
             return 0;
         }
