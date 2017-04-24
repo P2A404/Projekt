0049438567ef8c323a90764ad8253f2e86f87da6
EbbeLau <ebarge16@student.aau.dk>
2017-04-24 11:41:21 +0200

    Fucking virk, dit lorte Git(te)!

--- ArtificialNeuralNetwork/ArtificialNeuralNetwork/Tree.cs
+++ ArtificialNeuralNetwork/ArtificialNeuralNetwork/Tree.cs
@@ -10,34 +10,26 @@ namespace ArtificialNeuralNetwork
     {
         double[] GetChances(int BestOf, int HandicapHome, int HandicapOut, double ChanceHome, double ChanceOut)
         {
-            double HomeChance = 0;
-            double OutChance = 0;
-            double DrawChance = 0;
+            double homeChance = 0, outChance = 0, drawChance = 0;
             int TempBestOf = BestOf + HandicapHome + HandicapOut;
             int PseudoRound = HandicapOut + HandicapHome;
-            HomeChance = GetSingleTeamChance(HandicapHome, HandicapOut, TempBestOf, ChanceHome, ChanceOut, PseudoRound);
-
-            OutChance = GetSingleTeamChance(HandicapOut, HandicapHome, TempBestOf, ChanceOut, ChanceHome, PseudoRound);
-
-            DrawChance = 1 - HomeChance - OutChance;
-
-            double[] Chances = new double[3];
-            Chances[0] = HomeChance;
-            Chances[1] = OutChance;
-            Chances[2] = DrawChance;
-
+            homeChance = GetSingleTeamChance(HandicapHome, HandicapOut, TempBestOf, ChanceHome, ChanceOut, PseudoRound);
+            outChance = GetSingleTeamChance(HandicapOut, HandicapHome, TempBestOf, ChanceOut, ChanceHome, PseudoRound);
+            drawChance = 1 - homeChance - outChance;
+            double[] Chances = new double[3] { homeChance, outChance, drawChance };
             return Chances;
         }
 
         double GetSingleTeamChance(int HomeWon, int OutWon, int BestOf, double ChanceHome, double ChanceOut, int Round)
         {
-            if (HomeWon == (BestOf/2) + 1) { return 1; }
-            else if (OutWon == (BestOf / 2) + 1 || Round == BestOf) { return 0; }
+            if (HomeWon == (BestOf/2) + 1)
+            { return 1; }
+            else if (OutWon == (BestOf / 2) + 1 || Round == BestOf)
+            { return 0; }
             else
             {
                 double ReturnValue = ChanceHome * GetSingleTeamChance(HomeWon + 1 , OutWon, BestOf, ChanceHome, ChanceOut, Round)
-                    + ChanceOut * GetSingleTeamChance(HomeWon + 1, OutWon, BestOf, ChanceHome, ChanceOut, Round);
-                return ReturnValue;
+                    + ChanceOut * GetSingleTeamChance(HomeWon, OutWon + 1, BestOf, ChanceHome, ChanceOut, Round);
             }
         }
     }
