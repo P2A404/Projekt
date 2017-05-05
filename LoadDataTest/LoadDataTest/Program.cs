6fb9378ab052f61ca326db5b3c3084f51cbf4cf4
Rasmus Christensen <slayeren3024@gmail.com>
2017-05-05 10:40:27 +0200

    CSV Loading for Playerpool

--- LoadDataTest/LoadDataTest/Program.cs
+++ LoadDataTest/LoadDataTest/Program.cs
@@ -11,15 +11,13 @@ namespace LoadDataTest
     class Program
     {
         static void Main(string[] args)
-        {
-            // Time taking for the loading process
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            
+        {                      
             // New instance of Loading data
             LoadingData Load = new LoadingData();
-
+                 
+            // Getting Local path on this PC for GitHub
             string LocalPath = Load.GetLocalDirectory();
-
+            
             // Data Paths
             string ChampionDataPath = LocalPath + @"Data\CSV Files Champion Data";
             string EsportsDataPath = LocalPath + @"Data\CSV Files Lol Esport Data";
@@ -29,7 +27,6 @@ namespace LoadDataTest
             int NOFilesInChampion = Load.GetFileNames(ChampionDataPath).Length;
             int NOFilesInEsports = Load.GetFileNames(EsportsDataPath).Length;
             int NOFilesInMatchups = Load.GetFileNames(MatchupsDataPath).Length;
-            int AllMatches = NOFilesInChampion + NOFilesInEsports + NOFilesInMatchups;
 
             // New arrays with 2d array where the data should be stored
             string[][,] ChampionData = new string[NOFilesInChampion][,];
@@ -41,34 +38,22 @@ namespace LoadDataTest
             EsportsData = Load.LoadPath(EsportsDataPath);
             MatchupsData = Load.LoadPath(MatchupsDataPath);
 
+            // Declaring new double data arrays
             double[][,] DoubleChampionData = new double[NOFilesInChampion][,];
             double[][,] DoubleEsportsData = new double[NOFilesInEsports][,];
             double[][,] DoubleMatchupsData = new double[NOFilesInMatchups][,];
 
-
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-
-            //Console.WriteLine("Number of files loaded: " + AllMatches);
-            //Console.WriteLine("done it took " + elapsedMs + " milliseconds to load the data");
-
-            //Load.ReadJson(@"C: \Users\Rasmus\Desktop\Uni\P2\Git\Projekt\Data\Player Pool Data\out.json");
-
-            
-            
-
+            Load.ReadJson(LocalPath + @"Data\Player Pool Data\out.json");        
+   
             DoubleChampionData = Load.ConvertToIntArray(ChampionData);
 
-            //Console.WriteLine(ChampionData[0][6,11]);
+
+
+            // New instance of Matchups array
+            //Team[] TeamInfo = new Team[NOFilesInMatchups];
+
             
-               
-
-
-
-
-
-
-
+            
 
 
             Console.ReadKey();
---
+++ LoadDataTest/LoadDataTest/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadDataTest
+{
+    class Team
+    {
+        //private string TeamName;
+        //private int NumberOfWins;
+        //private int NumberOfLosses;
+
+
+
+    }
+
+}
