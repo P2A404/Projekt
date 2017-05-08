a4d446dc39ba6679c69f0853f0add53c4bde64db
Rasmus Christensen <slayeren3024@gmail.com>
2017-05-08 13:40:43 +0200

    Added Json Reader to project

---
+++ JsonReader/JsonReader/PlayerPool.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonReader
+{
+    public class Item
+    {
+        public int millis;
+        public string stamp;
+        public DateTime datetime;
+        public string light;
+        public float temp;
+        public float vcc;
+    }
+
+
+}
