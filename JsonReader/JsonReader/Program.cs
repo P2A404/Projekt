fa28efeaf720b4eede5c072f11ecf78754a7a5c6
Rasmus Christensen <slayeren3024@gmail.com>
2017-05-08 13:40:43 +0200

    Added Json Reader to project

---
+++ JsonReader/JsonReader/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JsonReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameInfo GameReader = new GameInfo();
+            List<JObject> matches = new List<JObject>();
+
+            for (int i = 0; i < 2827; i++)
+            {
+                using (StreamReader r = new StreamReader($@"C:\Users\Rasmus\Desktop\{i}.json"))
+                {
+                    string json = r.ReadToEnd();
+                    dynamic array = JsonConvert.DeserializeObject(json);
+                    matches.Add(array);
+                    Console.WriteLine("{0} {1} {2}", array[0].gameId, array.mapId, array.GetType());
+
+                    Console.Read();
+                }
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ JsonReader/JsonReader/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonReader
+{
+    public class Test
+    {
+        public class Account
+        {
+            public string Email { get; set; }
+            public bool Active { get; set; }
+            public DateTime CreatedDate { get; set; }
+            public IList<string> Roles { get; set; }
+        }
+    }
+}
