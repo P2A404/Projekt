014e071e2e25c0fe3efc217d01b4b7d71014194c
MrLongbottom <perich16@student.aau.dk>
2017-05-10 13:30:07 +0200

    cant remember changes.

--- JsonReader/JsonReader/Program.cs
+++ JsonReader/JsonReader/Program.cs
@@ -23,28 +23,22 @@ namespace JsonReader
 
         static void Main(string[] args)
         {
-            List<GameInfo.Match> Matches = new List<GameInfo.Match>();
+            NNInputFormatter formatter = new NNInputFormatter();
 
             string LocalPath = GetLocalDirectory() + @"Data\Matches\";
-
             for (int index = 1; index < 2827; index++)
             {
                 using (StreamReader Reader = new StreamReader(LocalPath + $@"{index}.json"))
                 {
                     // Current file read to the end
                     string json = Reader.ReadToEnd();
-
-                    //Dynamic = Json Object and loading things into object
                     var result = JsonConvert.DeserializeObject<GameInfo.Match>(json);
-                    
-                    
-
                     // Adding to final list of matches
-                    Matches.Add(result);
+                    formatter.matches.Add(result);
                     Console.WriteLine(index + " done");
                 }
             }
-            
+            formatter.ConvertGames();
             Console.Read();
         }
     }
