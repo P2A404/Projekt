d127dca549fb613fed00d4fb5c3b9b5cad5c9fba
MrLongbottom <perich16@student.aau.dk>
2017-05-10 13:51:06 +0200

    Made main pretty

--- JsonReader/JsonReader/Program.cs
+++ JsonReader/JsonReader/Program.cs
@@ -14,31 +14,9 @@ namespace JsonReader
 {
     class Program
     {
-        static public string GetLocalDirectory()
-        {
-            string LocalDirectory = Assembly.GetExecutingAssembly().Location;
-            LocalDirectory = LocalDirectory.Remove(Regex.Match(LocalDirectory, "JsonReader").Index);
-            return LocalDirectory;
-        }
-
         static void Main(string[] args)
         {
             NNInputFormatter formatter = new NNInputFormatter();
-
-            string LocalPath = GetLocalDirectory() + @"Data\Matches\";
-            for (int index = 1; index < 2827; index++)
-            {
-                using (StreamReader Reader = new StreamReader(LocalPath + $@"{index}.json"))
-                {
-                    // Current file read to the end
-                    string json = Reader.ReadToEnd();
-                    var result = JsonConvert.DeserializeObject<GameInfo.Match>(json);
-                    // Adding to final list of matches
-                    formatter.matches.Add(result);
-                    Console.WriteLine(index + " done");
-                }
-            }
-            formatter.ConvertGames();
             Console.Read();
         }
     }
