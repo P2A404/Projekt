919cc73bf456980051a239e4d7a21043bf0ad65c
MrLongbottom <perich16@student.aau.dk>
2017-05-17 14:15:39 +0200

    first test done?

--- ArtificialNeuralNetwork/ArtificialNeuralNetworkTests/NeuralNetworkTests.cs
+++ ArtificialNeuralNetwork/ArtificialNeuralNetworkTests/NeuralNetworkTests.cs
@@ -8,19 +8,35 @@ using System.Threading.Tasks;
 
 namespace ArtificialNeuralNetwork.Tests
 {
+    //Arrange - Gør test cases klar
+    //Act - Kør koden med test cases
+    //Assert - Tjek om antagelser omkring test cases er korrekte
     [TestFixture()]
     public class NeuralNetworkTests
     {
+        [TestFixtureSetUp()]
+        public void SetupSuite()
+        {
+
+        }
         [Test()]
         public void PrintArrayTest()
         {
             Assert.Fail();
         }
 
+        private double[] tf(double[] input) { return input; }
         [Test()]
         public void SumTest()
         {
-            Assert.Fail();
+            //Arrage
+            double[] input = new double[] { 0 };
+            double[,] weights = new double[1, 1] { { 0 } };
+            NeuralNetwork nn = new NeuralNetwork(new int[] { 0 }, tf, tf, tf, tf);
+            //Act
+            double[] result = nn.Sum(input, weights);
+            //Asser
+            Assert.AreEqual(new double[]{ 0 }, result);
         }
 
         [Test()]
